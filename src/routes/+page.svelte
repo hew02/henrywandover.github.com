65d01b5f512ea0d8577c260896f03a8e9e40c19b
tsarsalesman <hw9692@bard.edu>
2024-01-10 17:56:47 -0500

    fixed meta tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <svelte:head>
-    <meta charset="utf-8" />
-    <link rel="icon" href="%sveltekit.assets%/favicon.png" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Henry Wandover</title>
     <meta name="title" content="Henry Wandover" />
     <meta name="description" content="A portfolio page made by Henry Wandover"/>
     <meta name="keywords" content="Politics, Computer Science"/>
+    <meta name="author" content="Henry Wandover">
 </svelte:head>
