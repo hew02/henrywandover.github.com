e613c0306ae905294e776605206c2f6ab7e345f5
tsarsalesman <hw9692@bard.edu>
2024-01-10 17:53:01 -0500

    dd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <svelte:head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%sveltekit.assets%/favicon.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Henry Wandover</title>
-    <meta 
-        name="A student who writes about politics, as well as codes, 
-        living in America"
-        content="A portfolio page"
-    />
+    <meta name="title" content="Henry Wandover" />
+    <meta name="description" content="A portfolio page made by Henry Wandover"/>
+    <meta name="keywords" content="Politics, Computer Science"/>
 </svelte:head>
