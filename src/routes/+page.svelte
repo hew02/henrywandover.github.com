39331b539f650e2ea82ef7461240ffccc8ea5fa6
tsarsalesman <hw9692@bard.edu>
2024-01-10 16:16:53 -0500

    added metadata

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
 <svelte:head>
     <title>Henry Wandover</title>
+    <meta 
+        name="A student who writes about politics, as well as codes, 
+        living in America"
+        content="A portfolio page"
+    />
 </svelte:head>
