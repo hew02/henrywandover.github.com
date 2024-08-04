873c561fc5b305e72b5199fc959e6ef2a833c928
yung-turabian <henrywandover@gmail.com>
2024-08-04 18:58:00 -0400

    upt

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -41,7 +41,7 @@
 			<h3>Code</h3>
 			<ul class="left_list">
 						<a 
-						href="https://www.google.com"
+						href="https://yung-turabian.github.io/xtralib/"
 						target="_blank"
 						rel="noreferrer noopener"
 						title="View Doxygen pages for my lib">
