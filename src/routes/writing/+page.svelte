c795bf1e86c290af8139c2fd796db24115290eb3
Henry <77937076+yung-turabian@users.noreply.github.com>
2025-01-13 12:06:35 -0500

    prerender writings

--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -1,57 +1,61 @@
 <script>
-	import document from '$lib/docs/How_to_Create_a_Surveillance_State__And_How_Librarians_Attempted_to_Dismantle_It.pdf';
+
+export const prerender = true;
+export const csr = false;
+
+import document from '$lib/docs/How_to_Create_a_Surveillance_State__And_How_Librarians_Attempted_to_Dismantle_It.pdf';
 </script>
 
 
 <svelte:head>
-    <title>My Writing | Henry Wandover</title>
-    <meta name="title" content="Henry Wandover's Writing" />
-    <meta
-        name="description"
-        content="A personal site made by Henry Wandover, for Henry Wandover"
-    />
-    <meta name="keywords" content="Computer Science, Writing, Blog" />
-    <meta name="author" content="Henry Wandover" />
+<title>My Writing | Henry Wandover</title>
+<meta name="title" content="Henry Wandover's Writing" />
+<meta
+name="description"
+content="A personal site made by Henry Wandover, for Henry Wandover"
+/>
+<meta name="keywords" content="Computer Science, Writing, Blog" />
+<meta name="author" content="Henry Wandover" />
 </svelte:head>
 
 <div class="content">
 <h2>My writing (essays/poetry/articles)</h2>
 
 <ol class="none">
+<li>
+<a
+href="https://henrywandover.substack.com/p/some-poems"
+target="_blank"
+rel="noreferrer noopener"
+title="Substack article"
+dir="rtl"
+		>
+כמה שירים (Some Poems)
+		</a>
+		<p>Winter '24</p>
+		</li>
 		<li>
-        <a
-            href="https://henrywandover.substack.com/p/some-poems"
-            target="_blank"
-			rel="noreferrer noopener"
-			title="Substack article"
-			dir="rtl"
-        >
-            כמה שירים (Some Poems)
-        </a>
-        <p>Winter '24</p>
-    </li>
-    <li>
-        <a
-            href="https://henrywandover.substack.com/p/the-final-fumes-of-the-rutgers-teachers"
-            target="_blank"
-			rel="noreferrer noopener"
-			title="Substack article"
-        >
-            The Final Fumes of the Rutgers Teachers Strike
-        </a>
-        <p>Spring '23</p>
-    </li>
+		<a
+		href="https://henrywandover.substack.com/p/the-final-fumes-of-the-rutgers-teachers"
+		target="_blank"
+		rel="noreferrer noopener"
+		title="Substack article"
+		>
+		The Final Fumes of the Rutgers Teachers Strike
+		</a>
+		<p>Spring '23</p>
+		</li>
 
-    <li>
-	    <a
-            href={document}
-            target="_blank"
-			rel="noreferrer noopener"
-			title="PDF attachment">
+		<li>
+		<a
+		href={document}
+		target="_blank"
+		rel="noreferrer noopener"
+		title="PDF attachment">
 
-            On Creating a Surveillance State
-	    </a>
-        <p>Fall '23</p>
-    </li>
-</ol>
-</div>
+		On Creating a Surveillance State
+		</a>
+		<p>Fall '23</p>
+		</li>
+		</ol>
+		</div>
