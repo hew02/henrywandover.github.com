216d217c02f115b71ab41915ed697d78d0a855ad
Henry Wandover <henrywandover@gmail.com>
2025-01-13 17:58:36 -0500

    fixed typos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,17 +17,20 @@
 	<p>
     I'm currently a student at Bard College.
     I write code at school, as well as my home in Philadelphia.
-	With only one semester left to go I'm currently looking for work, either in software or game development.
-	If you have an oppurtunity, or know of one, check out my <a href="/Resume.pdf" hreflang="en" target="_blank" rel="noreferrer noopener" type="application/pdf">résumé</a>
+	With only one semester left to go, I'm currently looking for work, either in software or game development.
+	If you have an opportunity, or know of one, check out my <a href="/Resume.pdf" hreflang="en" target="_blank" rel="noreferrer noopener" type="application/pdf">résumé</a>
 	and the rest of my site.
 	</p>
 
 	<p>
-		The ideal way of getting in contact is by email: <a aria-label="Social media link" href="mailto:henrywandover@gmail.com" rel="noopener noreferrer" title="Email">henrywandover [at] gmail.com</a>. I don't really have much in the way of social media aside from <a aria-label="Social media link" href="https://www.linkedin.com/in/henrywandover/" target="_blank" rel="noreferrer noopener" title="LinkedIn">LinkedIn</a> (which I am not a huge fan of). There are more programming related things on <a aria-label="Social media link" href="https://github.com/yung-turabian" target="_blank" rel="noreferrer noopener" title="GitHub">GitHub</a> and more artistic endeavours on my <a aria-label="Social media link" href="https://www.youtube.com/@yungturabian" target="_blank" rel="noreferrer noopener" title="YouTube">YouTube</a>.
+		The ideal way of getting in contact is by email: <a aria-label="Social media link" href="mailto:henrywandover@gmail.com" rel="noopener noreferrer" title="Email">henrywandover [at] gmail.com</a>.
+        I don't really have much in the way of social media aside from <a aria-label="Social media link" href="https://www.linkedin.com/in/henrywandover/" target="_blank" rel="noreferrer noopener" title="LinkedIn">LinkedIn</a>
+        (which I am not a huge fan of). There are more programming-related things on <a aria-label="Social media link" href="https://github.com/yung-turabian" target="_blank" rel="noreferrer noopener" title="GitHub">GitHub</a>
+        and more artistic endeavours on my <a aria-label="Social media link" href="https://www.youtube.com/@yungturabian" target="_blank" rel="noreferrer noopener" title="YouTube">YouTube</a>.
  </p>
 
  <p>
-		As is tradition with personal sites I have curated a page of external <a href="/links" title="Sites I Enjoy">sites</a> for myself and others to browse.
+		As is tradition with personal sites, I have curated a page of external <a href="/links" title="Sites I Enjoy">sites</a> for myself and others to browse.
  </p>
 
 
