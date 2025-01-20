367aa29484291233d7d3d2e6a03bb21f4ef6cc2e
Henry Wandover <henrywandover@gmail.com>
2025-01-20 16:26:19 -0500

    changed page.ts -> +page.ts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,41 +1,41 @@
 <script lang="ts">
- import { onMount } from 'svelte';
- import { page } from '$app/stores';
- import { base } from '$app/paths';
+import { onMount } from 'svelte';
+import { page } from '$app/stores';
+import { base } from '$app/paths';
 
 
 let showSite = true;
 
- onMount(() => {
+onMount(() => {
 
-	});
+				});
 
 
-	function preload(src) {
+function preload(src) {
 		return new Promise(function (resolve) {
-				let img = new Image();
-				img.onload = resolve;
-				img.src = src;
-		});
-	}
+						let img = new Image();
+						img.onload = resolve;
+						img.src = src;
+						});
+}
 
-  if (typeof window !== 'undefined') {
+if (typeof window !== 'undefined') {
 		window.addEventListener('navigate-to', () => {
-				showSite = !showSite;
-		});
-	}
+						showSite = !showSite;
+						});
+}
 
 </script>
 
 <svelte:head>
-	<link rel="stylesheet" href="/app.css" />
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
-		rel="stylesheet"
-	/>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.11.1/p5.js"></script>
+<link rel="stylesheet" href="/app.css" />
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
+rel="stylesheet"
+/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.11.1/p5.js"></script>
 </svelte:head>
 
 {#if !$page.url.pathname.startsWith('/blog/')}
@@ -44,40 +44,40 @@ let showSite = true;
 </script>
 <noscript>
 </noscript>
- </div>
+</div>
 {/if}
 
 {#if showSite == true && $page.url.pathname != "/sand"}
 <div id="main-container">
 <div id="card">
-	<nav>
-		<a
-			href="/writing" class="item" class:active={$page.url.pathname == "/writing"}>
-			Writing
-		</a>
-		<a  id="nav-home"
-			href="/" class="item" class:active={$page.url.pathname == "/"}>
-			Home
-		</a>
-		<a
-			href="/code" class="item" class:active={$page.url.pathname == "/code"}>
-			Programming
-		</a>
-		<a
-			href="/blog" class="item" class:active={$page.url.pathname == "/blog"}
-			title="Blog posts">
-			Blog
-		</a>
-	</nav>
+<nav>
+<a
+href="/writing" class="item" class:active={$page.url.pathname == "/writing"}>
+Writing
+</a>
+<a  id="nav-home"
+href="/" class="item" class:active={$page.url.pathname == "/"}>
+Home
+</a>
+<a
+href="/code" class="item" class:active={$page.url.pathname == "/code"}>
+Programming
+</a>
+<a
+href="/blog" class="item" class:active={$page.url.pathname == "/blog"}
+title="Blog posts">
+Blog
+</a>
+</nav>
 
-	<slot />
+<slot />
 
 </div>
 
 <footer>
-	<ul>
-			<li>© 2025 Henry Wandover.</li>
-		</ul>
+<ul>
+<li>© 2025 Henry Wandover.</li>
+</ul>
 </footer>
 
 </div>
