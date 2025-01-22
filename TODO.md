ced062f460a49cf7280ce2d11a8688fef9bd8007
Henry Wandover <henrywandover@gmail.com>
2025-01-22 00:40:16 -0500

    using ssg now

--- TODO.md
+++
@@ -1,3 +0,0 @@
-# TODO
-
-- Write a blog on creating an svn/apache2 httpd server.
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,8 +9,7 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
 
 	kit: {
-		adapter: adapter({
-		})
+		adapter: adapter({})	
 	},
 
 	extensions: ['.svelte', '.svx']
