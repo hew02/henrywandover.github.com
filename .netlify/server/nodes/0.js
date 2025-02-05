ade4d68e4485d8ad47ae20f28d6ec4283971e8d6
Henry <77937076+yung-turabian@users.noreply.github.com>
2025-02-05 08:41:52 -0500

    Delete .netlify directory

--- .netlify/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.j0_4bxPT.js","_app/immutable/chunks/disclose-version.Bg9kRutz.js","_app/immutable/chunks/legacy.CtaTdtmd.js","_app/immutable/chunks/index.BFqC5wTN.js","_app/immutable/chunks/runtime.DlFo7mDq.js","_app/immutable/chunks/svelte-head.u4a9YmdR.js","_app/immutable/chunks/template.CsLgkFsT.js","_app/immutable/chunks/store.DUOL4G7C.js","_app/immutable/chunks/lifecycle.DdRORi1h.js","_app/immutable/chunks/index-client.F3P70AC7.js","_app/immutable/chunks/entry.nrO81_xg.js"];
-export const stylesheets = [];
-export const fonts = [];
