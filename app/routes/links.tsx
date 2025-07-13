5354a5c1f3c030194e67931dd63f6ef1b8d6b69b
Henry <77937076+yung-turabian@users.noreply.github.com>
2025-07-13 17:27:02 -0400

    Updated links page and changed the animation for the back button

--- app/routes/links.tsx
+++ app/routes/links.tsx
@@ -2,206 +2,257 @@ import Back from "../mods/back";
 import type { Route } from "./+types/links";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Cool Sites | Henry Wandover" },
-    { name: "description", content: "A personal site made by Henry Wandover, for Henry Wandover" },
-    { name: "keywords", content: "Computer Science, Writing, Blog" },
-    { name: "author", content: "Henry Wandover" },
-  ];
+    return [
+        { title: "Cool Sites | Henry Wandover" },
+        {
+            name: "description",
+            content:
+                "A personal site made by Henry Wandover, for Henry Wandover",
+        },
+        { name: "keywords", content: "Computer Science, Writing, Blog" },
+        { name: "author", content: "Henry Wandover" },
+    ];
 }
 
 export default function Links() {
-  return (
-    <div className="content">
-        <Back />
-        <h2>Links</h2>
+    return (
+        <div className="content">
+            <Back />
+            <h2>Links</h2>
 
-        <h3>General</h3>
+            <h3>General</h3>
 
-        <ul>
-
-            <li>
-            <a href="https://www.gjtorikian.com/Earthbound-Battle-Backgrounds-JS/?layer1=23&layer2=61" target="_blank" rel="noreferrer noopener"
-                    title="Earthbound battle background maker">Earthbound battle background maker</a>
-            </li>
-
-            <li>
-                <a
-                    href="https://int10h.org/oldschool-pc-fonts/fontlist/font?ibm_vga_8x16"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    title="IBM VGA font"
+            <ul>
+                <li>
+                    <a
+                        href="https://www.gjtorikian.com/Earthbound-Battle-Backgrounds-JS/?layer1=23&layer2=61"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="Earthbound battle background maker"
                     >
-                    The retro font used on this site
-                </a>
-            </li>
+                        Earthbound battle background maker
+                    </a>
+                </li>
 
-            <li>
-                <a
-                    href="https://www.puzzlescript.net/index.html"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    title="Puzzlescript"
+                <li>
+                    <a
+                        href="https://int10h.org/oldschool-pc-fonts/fontlist/font?ibm_vga_8x16"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="IBM VGA font"
                     >
-                    Puzzlescript
-                </a> a fun & simple game engine for making <em>sokoban</em> games
-            </li>
+                        The retro font used on this site
+                    </a>
+                </li>
 
-            <li>
-                <a
-                    href="https://www.sourcewatch.org/index.php?title=SourceWatch"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    title="SourceWatch"
+                <li>
+                    <a
+                        href="https://www.puzzlescript.net/index.html"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="Puzzlescript"
                     >
-                    SourceWatch
-                </a>, for keeping an eye on the corporate world
-            </li>
+                        Puzzlescript
+                    </a>{" "}
+                    a fun & simple game engine for making <em>sokoban</em> games
+                </li>
 
-            <li>
-                <a
-                    href="https://www.unionjobs.com/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    title="unionjobs.com"
+                <li>
+                    <a
+                        href="https://www.sourcewatch.org/index.php?title=SourceWatch"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="SourceWatch"
                     >
-                    Union Jobs Clearinghouse
-                </a> is a good platform for finding union employment
-            </li>
-            <li>
-                <a
-                    href="https://projecteuler.net/about"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    title="projecteuler.com"
-                    >
-                    project euler
-                </a>
-            </li>
-            <li>
-                <a
-                    href="https://www.codeabbey.com/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    title="codeabbey.com"
-                    >
-                    code abbey
-                </a>
-            </li>
-            <li>
-                <a
-                    href="https://www.bartleby.com/lit-hub/the-elements-of-style/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    title="elements-of-style-pdf"
-                    >
-                    The Elements of Style
-                </a>
-            </li>
-            <li>
-                <a
-                    href="https://script-fu.github.io/funky/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    title="Funky"
-                    >
-                                    Great stater resource on using Script-Fu for GIMP
-                </a>
-            </li>
-            <li>
-                <a
-                    href="https://beej.us/guide/bgc/html/split/index.html"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    title="Beej"
-                    >
-                                    Beej's Guide to C Programming	
-                </a>
-            </li>
+                        SourceWatch
+                    </a>
+                    , for keeping an eye on the corporate world
+                </li>
 
-            <li>
-                <a
-                    href="https://www.cs.cornell.edu/home/kleinber/networks-book/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    title="Networks"
+                <li>
+                    <a
+                        href="https://www.unionjobs.com/"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="unionjobs.com"
                     >
-                                    Networks, Crowds, and Markets: 	
-                </a>
-            </li>
-
-        </ul>
-
-        <br />
-
-        <h3>Academic Databases</h3>
-
-        <ul>
-
-            <li>
-                <a
-                    href="https://www.hathitrust.org/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    title="Hathitrust"
+                        Union Jobs Clearinghouse
+                    </a>{" "}
+                    is a good platform for finding union employment
+                </li>
+                <li>
+                    <a
+                        href="https://projecteuler.net/about"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="projecteuler.com"
                     >
-                    Hathitrust
-                </a>
-            </li>
-
-            <li>
-                <a
-                    href="https://www.jstor.org/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    title="IBM VGA font"
-                    >JSTOR</a>, <i>classic</i>
-            </li>
-
-            <li>
-                <a
-                    href="https://home.heinonline.org/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    title="Puzzlescript"
+                        project euler
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://www.codeabbey.com/"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="codeabbey.com"
                     >
-                    HeinOnline
-                </a>
-            </li>
-
-            <li>
-                <a
-                    href="https://www.librarystack.org/?lbry=public"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    title="LIBRARYSTACK"
+                        code abbey
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://www.bartleby.com/lit-hub/the-elements-of-style/"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="elements-of-style-pdf"
                     >
-                    LIBRARYSTACK
-                </a>
-            </li>
-        </ul>
+                        The Elements of Style
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://script-fu.github.io/funky/"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="Funky"
+                    >
+                        Great stater resource on using Script-Fu for GIMP
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://beej.us/guide/bgc/html/split/index.html"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="Beej"
+                    >
+                        Beej's Guide to C Programming
+                    </a>
+                </li>
+
+                <li>
+                    <a
+                        href="https://www.cs.cornell.edu/home/kleinber/networks-book/"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="Networks"
+                    >
+                        Networks, Crowds, and Markets
+                    </a>
+                </li>
+
+                <li>
+                    <a
+                        href="https://en.wikipedia.org/wiki/User:Hew02"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="My Wikipedia user page"
+                    >
+                        My Wikipedia user page
+                    </a>
+                </li>
+            </ul>
+
+            <br />
+
+            <h3>Academic Databases</h3>
+
+            <ul>
+                <li>
+                    <a
+                        href="https://www.hathitrust.org/"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="Hathitrust"
+                    >
+                        Hathitrust
+                    </a>
+                </li>
+
+                <li>
+                    <a
+                        href="https://www.jstor.org/"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="IBM VGA font"
+                    >
+                        JSTOR
+                    </a>
+                    , <i>classic</i>
+                </li>
+
+                <li>
+                    <a
+                        href="https://home.heinonline.org/"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="Puzzlescript"
+                    >
+                        HeinOnline
+                    </a>
+                </li>
+
+                <li>
+                    <a
+                        href="https://www.librarystack.org/?lbry=public"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="LIBRARYSTACK"
+                    >
+                        LIBRARYSTACK
+                    </a>
+                </li>
+            </ul>
 
             <br />
 
             <h3>Type Theory</h3>
 
-        <ul>
+            <ul>
+                <li>
+                    <a
+                        href="https://kar.kent.ac.uk/20998/1/ttfp.pdf"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="type-theory-and-functional-programming"
+                    >
+                        Type Theory & Functional Programming
+                    </a>{" "}
+                    by Simon Thompson
+                </li>
 
-            <li><a href="https://kar.kent.ac.uk/20998/1/ttfp.pdf" target="_blank" rel="noreferrer noopener"
-                    title="type-theory-and-functional-programming">Type Theory & Functional Programming</a> by Simon Thompson</li>
-        </ul>
+                <li>
+                    A{" "}
+                    <a
+                        href="https://github.com/hew02/awesome-type-theory
+"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="awesome-type-theory"
+                    >
+                        GitHub repo
+                    </a>{" "}
+                    of resources I am putting together
+                </li>
+            </ul>
 
             <br />
 
             <h3>Algorithms</h3>
 
-        <ul>
-
-            <li><a href="https://www.youtube.com/playlist?list=PLKX7b2H72fI_uRam3mZbNC7-6TcwTltVd" target="_blank" rel="noreferrer noopener"
-                    title="Helpful videos on aglorithms">YouTube playlist</a></li>
-        </ul>
-
-
-    </div>
-  );
+            <ul>
+                <li>
+                    <a
+                        href="https://www.youtube.com/playlist?list=PLKX7b2H72fI_uRam3mZbNC7-6TcwTltVd"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="Helpful videos on aglorithms"
+                    >
+                        YouTube playlist
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
 }
